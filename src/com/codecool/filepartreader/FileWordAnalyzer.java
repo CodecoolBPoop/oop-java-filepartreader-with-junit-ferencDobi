55d7b7cf4f9f7025fea9576b68e26037286086dc
ferencDobi <ferencdbi@gmail.com>
2018-09-08 15:49:17 +0200

    analyzer fix to pass tests

--- src/com/codecool/filepartreader/FileWordAnalyzer.java
+++ src/com/codecool/filepartreader/FileWordAnalyzer.java
@@ -2,6 +2,7 @@ package com.codecool.filepartreader;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -14,20 +15,25 @@ public class FileWordAnalyzer {
     }
 
     private Stream<String> toStream() {
-        return Arrays.stream(reader.readLines().split("//W+"));
+        return Arrays.stream(reader.readLines().split("[^\\w']+"));
     }
 
     public List<String> wordsByABC() {
-        return toStream().sorted().collect(Collectors.toCollection(ArrayList::new));
+        return toStream().sorted(Comparator.comparing(String::toLowerCase))
+                         .collect(Collectors.toCollection(ArrayList::new));
     }
 
-    public List<String> wordsContainingSubString (String subString) {
+    public List<String> wordsContainingSubString(String subString) {
         return toStream().filter(word -> word.matches(".*" + subString + ".*"))
                          .collect(Collectors.toCollection(ArrayList::new));
     }
 
-    public List<String> wordsArePalindrome () {
-        return toStream().filter(word -> word.equals(new StringBuilder(word).reverse().toString()))
+    public List<String> wordsArePalindrome() {
+        return toStream().filter(this::isPalindrome)
                          .collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    private boolean isPalindrome(String word) {
+        return word.toLowerCase().equals(new StringBuilder(word).reverse().toString().toLowerCase());
     }
 }
